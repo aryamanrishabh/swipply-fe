528eb09d42be6e967c5b99c6314090b617cd3a62
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-05-05 18:54:09 -0400

    add constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,2 +1,8 @@
+export const COMPANY = "company";
 export const CANDIDATE = "candidate";
 export const RECRUITER = "recruiter";
+
+// localStorage keys
+export const IGNORE_IDS = "ignoreIds";
+export const IGNORE_JOB_IDS = "ignoreIds";
+export const IGNORE_CANDIDATE_IDS = "ignoreIds";
