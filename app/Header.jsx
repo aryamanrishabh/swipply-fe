df2187a1964923e937f251db1db84e02db659cf4
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-05-07 17:48:58 -0400

    add logout

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,16 +1,30 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
+import { FiLogOut } from "react-icons/fi";
+import { usePathname, useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { logout } from "./redux/slices/authSlice";
 
 const Header = () => {
+  const router = useRouter();
   const path = usePathname();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => dispatch(logout({ router }));
 
   if (path?.includes?.("auth")) return null;
 
   return (
-    <div className="flex h-16 min-h-16 items-center justify-center  bg-blue-500">
-      Header
+    <div className="flex h-16 min-h-16 px-12 items-center justify-between  bg-blue-500">
+      <span>Header</span>
+
+      <FiLogOut
+        size="1.5rem"
+        color="white"
+        cursor="pointer"
+        onClick={handleLogout}
+      />
     </div>
   );
 };
