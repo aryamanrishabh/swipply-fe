d69879412bc2058f2c9950066876b65cc798797f
Aryaman Rishabh <aryamanrishabh@gmail.com>
2024-05-12 13:57:41 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: [
+      "swipply-resume.s3.amazonaws.com",
+      "swipply-profile-pictures.s3.amazonaws.com",
+    ],
+  },
   redirects: async () => {
     return [
       {
